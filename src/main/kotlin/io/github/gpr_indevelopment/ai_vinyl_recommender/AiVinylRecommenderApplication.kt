226998e241305ec1085b84ce75860a847856f809
Gabriel Robaina <gpr.indevelopment@gmail.com>
2024-06-09 22:21:21 -0300

    Setup Spring Boot app

---
+++ src/main/kotlin/io/github/gpr_indevelopment/ai_vinyl_recommender/AiVinylRecommenderApplication.kt
@@ -0,0 +1,11 @@
+package io.github.gpr_indevelopment.ai_vinyl_recommender
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiVinylRecommenderApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiVinylRecommenderApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/gpr_indevelopment/ai_vinyl_recommender/AiVinylRecommenderApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.gpr_indevelopment.ai_vinyl_recommender
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiVinylRecommenderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
