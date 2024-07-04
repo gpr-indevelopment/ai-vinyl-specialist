40acc2bee1926f02d99b730ab3b0dd3ad85c60d0
Gabriel Robaina <gpr.indevelopment@gmail.com>
2024-07-03 22:04:48 -0300

    Remove TODO for tagging

--- src/main/kotlin/io/github/gprindevelopment/recommender/AiVinylRecommenderApplication.kt
+++ src/main/kotlin/io/github/gprindevelopment/recommender/AiVinylRecommenderApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 //TODO: Global extension function for easy random generate random instance
-//TODO: Do git tagging for landmark commits: Discogs username field with llama3, with openAI, and then tooling enabled.
 @SpringBootApplication
 @EnableFeignClients
 class AiVinylRecommenderApplication
